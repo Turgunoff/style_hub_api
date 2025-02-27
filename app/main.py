93c16e50e852db5e8941a93e7d1f12c284d453fa
Turgunoff <eldor.turgunov.94@yandex.com>
2025-02-27 13:01:59 +0000

    Update application port to 7777 in main.py

--- app/main.py
+++ app/main.py
@@ -51,4 +51,4 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=7777, reload=True)
